13e22376f1084526e19c85cf1502df11bb0d19bc
muhamad agus <muhamadagus3197@gmail.com>
2023-03-02 22:39:15 +0700

    second commit

---
+++ android/app/src/main/kotlin/com/twitter/clone/twitter_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.twitter.clone.twitter_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppWriteConstants {
+  static const String databaseId = '6400c110a4a6e05373d6';
+  static const String projectId = '6400badc416631a639d3';
+  static const String endpoint = 'http://localhost:80/v1';
+}
